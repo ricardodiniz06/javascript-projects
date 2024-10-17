2a8f9b40c8ec4950019b962cebae133d26cf3769
ricardodiniz06 <rddsilva@minha.fag.edu.br>
2024-10-17 11:50:55 -0300

    Atualizacao

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,21 +2,21 @@ import { createStore, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';  // Com o uso de chaves
 import rootReducer from './reducers/rootReducer';
 
-// Importa as funções de localStorage
+
 import { loadState, saveState } from '../utils/localStorage';
 
-// Carrega o estado persistido do localStorage (se existir)
+
 const persistedState = loadState();
 
 const store = createStore(
     rootReducer,
-    applyMiddleware(thunk)  // Removemos temporariamente o composeWithDevTools
+    applyMiddleware(thunk)  // 
   );
 
-// Salva o estado no localStorage sempre que ele mudar
+
 store.subscribe(() => {
   saveState({
-    tasks: store.getState().todo.tasks, // Apenas salva as tarefas no localStorage
+    tasks: store.getState().todo.tasks, 
   });
 });
 
